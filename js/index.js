4a640ab8bfe4dec6fe8ff727be1130ffb51f4c06
Esceup <20danila.gorbunov.01@gmail.com>
2024-10-21 18:43:57 +0300

    Добавлены стили

--- js/index.js
+++ js/index.js
@@ -5,24 +5,49 @@ const nameTask = document.querySelector('.nameTask')
 const deleteTask = (event) => {
     const taskItem = event.target.parentNode;
     taskItem.remove();
+
+    
 }
 
 const addNewAction = () => {
     //добавления нового экшиона в туду листе
     const task = document.createElement('li');
-
+    task.classList.add('listItem')
     const nameTaskValue = nameTask.value;
 
-    task.innerHTML = `<input type='checkbox' class='task-select'> <span> ${nameTaskValue}</span> <button class='delete-task'>Remove</button>`;
-    const deleteBtn = task.querySelector('.delete-task');
-    deleteBtn.addEventListener('click', deleteBtn)
+    task.innerHTML = `<input type='checkbox'  class='select check'> 
+    <span class="taskValue"> ${nameTaskValue}</span> 
+    
+    <button class='deleteTask btn btnDelete' disabled> Удалить</button >`;
 
+    const deleteBtn = task.querySelector('.deleteTask');
+    deleteBtn.addEventListener('click', deleteTask);
+    
     list.appendChild(task);
 
     nameTask.value = '';
+
+    const check = task.querySelector('.check');
+    const removeBtn = task.querySelector('.btnDelete');
+    check.addEventListener('change', function () {
+        if (check.checked) {
+            removeBtn.disabled = false;
+        } else {
+            removeBtn.disabled = true;
+        }
+    });
+    
 }
 
+
 btnAdd.addEventListener('click', addNewAction);
+
+
+
+
+
+
+
 
 //remove - 
 //style - 
